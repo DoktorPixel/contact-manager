0a562e0b0d0a57a7cf1931dec4184a47b351c8fe
Oleh Halchenko <oleh.halchenko@appsdelivered.com>
2024-08-05 14:22:53 +0200

    added env

--- src/api/contacts.ts
+++ src/api/contacts.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
 
-const API_KEY = "VlP9cwH6cc7Kg2LsNPXpAvF6QNmgZn";
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 const apiClient = axios.create({
-  baseURL: "https://live.devnimble.com/api/v1",
-  // "https://cors-anywhere.herokuapp.com/https://live.devnimble.com/api/v1",
+  baseURL:
+    // "/https://live.devnimble.com/api/v1",
+    "https://cors-anywhere.herokuapp.com/https://live.devnimble.com/api/v1",
   headers: {
     Authorization: `Bearer ${API_KEY}`,
   },
